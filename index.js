25664793a477ec5975036042a6cabffee3196b68
Graham Flaspoehler <grahamflas@gmail.com>
2019-11-04 19:53:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+// Group 1
 const farmAnimals = 'cow horse sheep pig chicken'
 
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(" ");
+const [Bessie, , Dolly, Babe, Little] = farmAnimals.split(" ");
+const [black_and_white, , black, pink, , ] = farmAnimals.split(" ");
+
+// Group 2
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+const [r, o, y, g, b, , v] = colors
+const [ , , , , , indg, ] = colors
+
+// Group 3
 const muppet = {
   name: 'Kermit',
   color: 'green',
@@ -9,6 +20,8 @@ const muppet = {
   job: 'Host of The Muppet Show',
   partner: 'Miss Piggy'
 };
+
+const {name, color, song, job, partner} = muppet
 
 const k_muppet = {
   k_name: 'Kermit',
@@ -24,6 +37,11 @@ const k_muppet = {
   k_job: 'Host of The Muppet Show',
   k_partner: 'Miss Piggy'
 };
+
+const {song_2, song_4} = k_muppet.k_album.the_muppet_movie
+const {k_job} = k_muppet
+const {k_partner} = k_muppet
+
 
 // Strings
 
